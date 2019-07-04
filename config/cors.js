2c360cb4668c0369f3d6244c2c703b7186cc6db4
Felipe Marcon <felipeemarcon@gmail.com>
2019-07-04 18:13:11 -0300

    Handle with CORS

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: false,
+  origin: ['felipemarcon.com.br', 'services.felipemarcon.com.br'],
 
   /*
   |--------------------------------------------------------------------------
